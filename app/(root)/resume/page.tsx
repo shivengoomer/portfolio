6b7781d3ae1c2fade9aabb7e16522755a1e017aa
Shiven Goomer <shivengoomer@gmail.com>
2026-05-21 18:47:55 +0530

    Update redirect URL for resume page

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function ResumePage() {
   useEffect(() => {
-    redirect("https://drive.google.com/file/d/1Yoj84KJHPR-TUEMuZWbRMbLJ2Tbu5qvA/view?usp=drive_link" || "/");
+    redirect("https://drive.google.com/file/d/1jHc0J6cnYG-eDUHVr2KBlMx0mr0Dxgpx/edit" || "/");
   }, []);
   return <div>Redirecting to the resume...</div>;
 }
